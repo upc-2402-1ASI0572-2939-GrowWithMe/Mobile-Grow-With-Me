fc8a533ab75f6e8652b8cab2ccc038c5e99940b8
TolyNoriega5 <125397518+TolyNoriega5@users.noreply.github.com>
2025-06-20 12:23:21 -0500

    feat: add farmer and consultant profile

---
+++ app/src/main/java/com/example/prueba/Profile/Beans/ConsultantProfile.kt
@@ -0,0 +1,48 @@
+package com.example.prueba.Profile.Beans
+
+import java.io.Serializable
+
+class ConsultantProfile : Serializable {
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var email: String
+    var phone: String
+    var photoUrl: String
+    var dni: String
+
+    constructor(
+        id: Int,
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        photoUrl: String,
+        dni: String
+    ) {
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.phone = phone
+        this.photoUrl = photoUrl
+        this.dni = dni
+    }
+
+    constructor(
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        photoUrl: String,
+        dni: String
+    ) {
+        this.id = 0
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.phone = phone
+        this.photoUrl = photoUrl
+        this.dni = dni
+    }
+}

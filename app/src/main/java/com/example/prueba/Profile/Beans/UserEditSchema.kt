c40669ea16c7733443a6a0b890d1a58c620276aa
TolyNoriega5 <125397518+TolyNoriega5@users.noreply.github.com>
2025-06-20 12:23:39 -0500

    feat: add user edit schema

---
+++ app/src/main/java/com/example/prueba/Profile/Beans/UserEditSchema.kt
@@ -0,0 +1,8 @@
+package com.example.prueba.Profile.Beans
+
+data class UserEditSchema (
+    var name: String,
+    var lastname: String,
+    var phonenumber: String,
+    var profile_image: String
+)

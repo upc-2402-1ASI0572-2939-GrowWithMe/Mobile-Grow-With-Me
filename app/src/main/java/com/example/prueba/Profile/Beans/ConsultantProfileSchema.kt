462cda77d8ad57c5d0b6ae8f1785b699a7bc162a
TolyNoriega5 <125397518+TolyNoriega5@users.noreply.github.com>
2025-06-20 12:23:27 -0500

    feat: add farmer and consultant profile schema

---
+++ app/src/main/java/com/example/prueba/Profile/Beans/ConsultantProfileSchema.kt
@@ -0,0 +1,10 @@
+package com.example.prueba.Profile.Beans
+
+data class ConsultantProfileSchema(
+    var firstName: String,
+    var lastName: String,
+    var phone: String,
+    var email: String,
+    var dni: String,
+    var photoUrl: String
+)
---
+++ app/src/main/java/com/example/prueba/Profile/Beans/FarmerProfileSchema.kt
@@ -0,0 +1,10 @@
+package com.example.prueba.Profile.Beans
+
+data class FarmerProfileSchema(
+    var firstName: String,
+    var lastName: String,
+    var phone: String,
+    var email: String,
+    var dni: String,
+    var photoUrl: String
+)
